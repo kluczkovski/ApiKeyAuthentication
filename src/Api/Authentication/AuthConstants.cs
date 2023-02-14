27ffd3c29658238695066b434d499866d2978c67
EvertonKluczkovski <kluczkovski@hotmail.com>
2023-02-14 22:38:25 +0000

    Using Middlaware approach for all controllers.

---
+++ src/Api/Authentication/AuthConstants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WebApi.Authentication
+{
+	public abstract class AuthConstants
+	{
+		public const string ApyKeySectionName = "Authentication:ApiKey";
+		public const string AuthKeyHeaderName = "X-Api-Key";
+	}
+}
+
